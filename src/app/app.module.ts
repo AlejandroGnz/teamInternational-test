909b7f8e870929eed14f3b5d4773ca4b2fb6556e
Alejandro <alejandromora622@gmail.com>
2018-11-03 19:55:01 -0500

    enabled browser animation module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,5 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
 
 import { MaterialModule } from './custom-modules/material.module';
@@ -12,6 +13,7 @@ import { EmployeesTableComponent } from './components/employees-table/employees-
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
     MaterialModule
   ],
   providers: [],
